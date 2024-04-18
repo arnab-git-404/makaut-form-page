45e496bd8f37e031218e7b0f5f1270db3736d487
Arnab Mukherjee <136512754+arnab-git-404@users.noreply.github.com>
2024-04-18 19:37:25 +0530

    Update ThirdSem.js

--- src/ThirdSem.js
+++ src/ThirdSem.js
@@ -3,7 +3,7 @@ import React from 'react'
 const ThirdSem = () => {
   return (
     <div>
-        <iframe tilte="3rd" 
+        <iframe tilte="3rd Semester Form" 
         src="https://docs.google.com/forms/d/e/1FAIpQLSdh-jXAsZ5Yg08r6KCZDinVnU9zIe8Ps7q5aw-q-3E4EIqIlw/viewform?embedded=true" 
          className=" md:w-[500px] lg:w-[800px] md:h-[750px]"
          height="500"
