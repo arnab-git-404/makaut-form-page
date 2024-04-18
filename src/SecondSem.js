e339e600c31240dec4652a4f43b8d3230a68377a
Arnab Mukherjee <136512754+arnab-git-404@users.noreply.github.com>
2024-04-18 19:41:04 +0530

    Update SecondSem.js

--- src/SecondSem.js
+++ src/SecondSem.js
@@ -3,7 +3,7 @@ import React from 'react'
 const SecondSem = () => {
   return (
     <div>
-        <iframe tilte="2nd Semester Form" 
+        <iframe title="2nd Semester Form" 
         src="https://docs.google.com/forms/d/e/1FAIpQLSfpzKpfD8G2RZe_cpjFeJSfKVkdeeJt1pcf2WKuFcVbwZpSCQ/viewform?embedded=true" 
          className=" md:w-[500px] lg:w-[800px] md:h-[750px]"
          height="500" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
